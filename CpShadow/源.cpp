632c19ab887b79cbd0c3edf56534f538ad3219e6
Elliott.Zheng <elliott.zheng@foxmail.com>
2018-01-09 15:53:40 +0800

    finish

--- CpShadow/源.cpp
+++ CpShadow/源.cpp
@@ -1,38 +1,82 @@
-#include <windows.h>
-#include <Wininet.h>
-#include <iostream> 
-using namespace std; 
-#pragma comment(lib, "Wininet.lib") 
+// rapidjson/example/simpledom/simpledom.cpp`
+#include "rapidjson/document.h"
+#include "rapidjson/writer.h"
+#include "rapidjson/stringbuffer.h"
+#include <cassert>
+#include <iostream>
+#include<fstream>
+#include<string>
+#include<sstream>
+using namespace std;
 
-void Download(char url[]); 
-int main(int argc, TCHAR* argv[], TCHAR* envp[])
-{
-	int nRetCode = 0; char url[1000];//存放URL地址 //用户键入quit时退出 
-	while(cin >> url && strcmp(url, "quit") != 0) 
-	{ 
-		try {
-			cout << "目标URL：" << url << endl; Download(url); 
-		} catch (char* e)//显示出错信息 
+#define CONFIG_FILE_NAME "gui-config.json"
+#define CONFIG_TEMPLATE_FILE_NAME "gui-config-template.json"
+
+using namespace rapidjson;
+int main() {
+	// 1. 把 JSON 解析至 DOM。
+	ifstream ifs(CONFIG_TEMPLATE_FILE_NAME,ios::in);
+	if (!ifs)
+		cerr << "ERROR:Can't open config file,make sure there are" <<CONFIG_TEMPLATE_FILE_NAME<<"in the same directory."<< endl;
+	std::stringstream string_buffer;
+	string_buffer << ifs.rdbuf();
+	ifs.close();
+	std::string contents(string_buffer.str());
+	cout << contents << endl;
+
+	Document document;
+	document.Parse(contents.c_str());
+	 
+	StringBuffer buffer;
+	Writer<StringBuffer> writer(buffer);
+	try 
+	{
+		rapidjson::Value& configs = document["configs"];
+		assert(configs.IsArray());
+		configs.Clear();
+		for (int i = 0; i < 10; i++)
 		{
-			cout << e << endl << endl; 
-		} 
-	} return nRetCode; 
-} 
-void Download(char url[]) 
-{
-	char buffer[100000];//下载文件的缓冲区 
-	DWORD bytes_read;//下载的字节数 //打开一个internet连接 
-	HINTERNET internet=InternetOpen("HTTP Downloader", INTERNET_OPEN_TYPE_PRECONFIG, NULL, NULL, NULL); 
-	if(!internet) 
-		throw "InternetOpen error!"; 
-	if(!internet) throw "InternetOpen error!"; //打开一个http url地址 
-	HINTERNET file_handle = InternetOpenUrl(internet, url, NULL, 0, INTERNET_FLAG_RELOAD, 0); 
-	if(!file_handle) 
-		throw "InternetOpenUrl error! - Maybe you should add Http:// or Ftp://"; //从url地址中读取文件内容到缓冲区buffer 
-	BOOL b = InternetReadFile(file_handle, buffer, 100000, &bytes_read); 
-	if(!b) 
-		throw "InternetReadFile error!"; 
-	buffer[bytes_read] = 0; 
-	cout << buffer << endl << endl; //关闭连接 
-	InternetCloseHandle(internet); 
+			Value item(Type::kObjectType);
+			string str = "c.isxb.bid";
+			str += to_string(i);
+			Value server(Type::kStringType);
+			server.SetString(StringRef(str.c_str()));
+			item.AddMember("sever", server, document.GetAllocator());
+			item.AddMember("server_port", "18008", document.GetAllocator());
+			item.AddMember("password", "12121691", document.GetAllocator());
+			item.AddMember("method","aes-256-cfb", document.GetAllocator());
+			item.AddMember("remarks","", document.GetAllocator());	
+			item.AddMember("auth","false", document.GetAllocator());		
+			item.AddMember("timeout","5", document.GetAllocator());		
+			configs.PushBack(item, document.GetAllocator());
+		}
+		document.Accept(writer);
+		// Output {"project":"rapidjson","stars":11}
+		ofstream ofs(CONFIG_FILE_NAME, ios::trunc);
+		if(!ofs)
+			cerr<<"Generate config file fail"<<endl;
+		ofs << buffer.GetString();
+		ofs.close();
+	}catch (exception e)
+	{
+	
+	}
+	
+
+
+	// 2. 利用 DOM 作出修改。
+	/*
+	Value& s = document["stars"];
+	s.SetInt(s.GetInt() + 1);
+	
+	// 3. 把 DOM 转换（stringify）成 JSON。
+	StringBuffer buffer;
+	Writer<StringBuffer> writer(buffer);
+	document.Accept(writer);
+	// Output {"project":"rapidjson","stars":11}
+	std::cout << buffer.GetString() << std::endl;
+	*/
+	ifs.close();
+	system("pause");
+	return 0;
 }
